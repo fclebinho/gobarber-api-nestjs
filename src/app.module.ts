7a9685c76003c197ebbb3f5dad61462d8531ee67
Cleber Gomes <fclebinho@gmail.com>
2023-08-09 22:54:40 -0300

    Add supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupabaseGuard, SupabaseModule } from './common/supabase';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), PassportModule, SupabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: SupabaseGuard,
+    },
+  ],
 })
 export class AppModule {}
